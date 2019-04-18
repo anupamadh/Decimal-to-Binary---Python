bbc7d64f4e5e80bcc7d4f0f1c0c0453a32f7a091
someone <someone@someplace.com>
2019-04-18 15:55:48 +0800

    Added functionality for binary to decimal

--- Decimal_to_Binary.py
+++ Decimal_to_Binary.py
@@ -8,7 +8,6 @@ This is a temporary script file.
 result =[]
 
 def decimal_to_binary(decimal):
-    print ("Hello")
     while (decimal  > 0):
         rem = int(decimal % 2)
         print (rem)
@@ -20,4 +19,19 @@ decimal = int(input("Enter the number in digital format: "))
 decimal_to_binary(decimal)
 
 #Binary to Decimal
+def binary_to_decimal(binary):
+    result_bin,dec = 0, 0
+    n=0
+    while (binary != 0):
+        dec = binary % 10
+        print("dec {}:".format(dec))
+        binary = binary//10
+        print("binary {}:".format(binary))
+        result_bin = result_bin + dec * pow(2, n)
+        print ("result_bin {}:".format(result_bin))
+        n +=1
+    print(result_bin)
 
+
+binary = int(input("Enter the number in binary format: "))
+binary_to_decimal(binary)
